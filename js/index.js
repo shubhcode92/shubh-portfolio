b20688585382817a5e885495fe844b546c0d0919
shubhcode92 <abc123@gmail.com>
2023-12-26 20:41:04 +0530

    Added my project

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings: ["","Java Developer","Problem Solver","Competitive Programmer","Web Designer"],
+    typeSpeed:150,
+    BackSpeed:60,
+    loop:true
+})

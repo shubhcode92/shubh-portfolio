19aea2550a2a920d7b7c6e9e0c671fad7ef71bcb
Shubham Sahu <105041611+shubhcode92@users.noreply.github.com>
2024-06-18 20:04:53 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing",{
-    strings: ["","Java Developer","Problem Solver","Competitive Programmer","Web Designer"],
+    strings: ["","Frontend Developer","Problem Solver","Competitive Programmer","Web Designer"],
     typeSpeed:150,
     BackSpeed:60,
     loop:true
